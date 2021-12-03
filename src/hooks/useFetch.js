01d07ec18edb92a624e1d0cfd233c3fbd9a32138
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2021-12-03 20:37:34 +0100

    Practicing hooks

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const controller = new AbortController();  // Create the controller to abort the fetch if the component its unmounted
+
+    const fetchData = async () => {
+      setIsPending(true);
+
+      try {
+        const res = await fetch(url, { signal: controller.signal });
+        if (!res.ok) {
+          // If url it's wrong, throw the error
+          throw new Error(res.statusText);
+        }
+        const json = await res.json();
+        setIsPending(false);
+        setError(null);
+        setData(json);
+      } catch (error) {
+        if (error.name === "AbortError") {
+          console.log("The fetch was aborted");
+        } else {
+          setIsPending(false);
+          setError("Could not fetch the data");
+          console.log(error);
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
